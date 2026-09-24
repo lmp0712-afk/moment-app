eb400fab0082631f13fe71ebbff0248ab35555e5
lmp0712-afk <lmp0712@web.de>
2026-09-24 21:30:48 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
-const CACHE="moment-v1"; self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"])))); self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
+const CACHE_NAME = 'moment-v4';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon-512.png', './apple-touch-icon.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
